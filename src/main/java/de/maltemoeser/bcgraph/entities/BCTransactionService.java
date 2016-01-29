1a8dc3473efb3713a1ca977e239cad4631943609
Malte Möser <mail@maltemoeser.de>
2016-01-29 18:09:03 +0100

    Explain different methods for getting transactions

--- src/main/java/de/maltemoeser/bcgraph/entities/BCTransactionService.java
+++ src/main/java/de/maltemoeser/bcgraph/entities/BCTransactionService.java
@@ -19,6 +19,13 @@ public class BCTransactionService extends BCEntityService {
         return bcTransaction;
     }
 
+    /**
+     * Retrieve a transaction by its hash.
+     * Note, that there can be multiple transactions with the same hash -- in this case they can only be spent according to BIP30.
+     * There have been transactions with duplicate hashes in the past, but they have not been spent, therefore
+     * it is currently possible to use this method during import in order to look up the outputs of previous transactions.
+     * Note, that this method will fail when it tries to read one of those transactions with duplicate hashes.
+     */
     public BCTransaction getTransactionByHash(String transactionHash) {
         Node result = IteratorUtil.single(
                 graphDatabaseService.findNodes(LabelType.Transaction, NodeProperty.TRANSACTION_HASH, transactionHash)
@@ -32,13 +39,14 @@ public class BCTransactionService extends BCEntityService {
      */
     public BCTransaction getTransactionByHashAndBlockHash(String transactionHash, String blockHash) {
         List<Node> nodes = IteratorUtil.asList(graphDatabaseService.findNodes(LabelType.Transaction, NodeProperty.TRANSACTION_HASH, transactionHash));
-        if(nodes.size() == 0) {
+        if(nodes.size() == 0) { // we did not find any node with this transaction hash
             throw new NoSuchElementException("No transaction found with hash " + transactionHash);
-        } else if(nodes.size() == 1) {
+        } else if(nodes.size() == 1) { // there is exactly one with this transaction hash
             return new BCTransaction(nodes.get(0));
-        } else {
+        } else { // there is more than one node with this transaction hash
             for(Node n : nodes) {
                 BCTransaction transaction = new BCTransaction(n);
+                // the transaction can be uniquely identified by comparing the hash of the block it is in
                 if(transaction.getBlock().getHash().equals(blockHash)) {
                     return transaction;
                 }
